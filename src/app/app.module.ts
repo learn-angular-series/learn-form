1daca2ca1209efe27992ae82c7fd69b1415ec8a3
damoqiongqiu <damoqiongqiu@126.com>
2017-09-28 10:00:41 +0800

    自定义校验规则

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChineseMobileValidator } from './form-quick-start/directives/Chinese-mobile-validator.directive';
 import { FormQuickStartComponent } from './form-quick-start/form-quick-start.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ChineseMobileValidator,
     FormQuickStartComponent
   ],
   imports: [
